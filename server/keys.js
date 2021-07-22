612e5b15493c4ca13b185c051dd02f8a0b85ffdc
Ketan Ahuja <56186989+KETANAHUJA@users.noreply.github.com>
2021-07-22 16:59:11 +0530

    Delete keys.js

--- server/keys.js
+++
@@ -1,6 +0,0 @@
-// secret stuff 
-module.exports={
-    MONGOURI:"mongodb+srv://ketan:0MuOWkwOlkAPM9UP@cluster0.pvaes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    // ANTHING RANDOM , WHICH NO ONE CAN GUESS 
-    JWT_SECRET : "ESgfdrtd4567#$$@!@@#TREDGJMNCXAAAssdg"
-}
